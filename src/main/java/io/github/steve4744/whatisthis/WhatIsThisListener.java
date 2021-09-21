62453fb8e88fa68caa54d5a8bfee6de4356d7a3c
TreeHD <mini87479@gmail.com>
2021-09-21 22:49:33 +0800

    import org.bukkit.event.player.PlayerMoveEvent

--- src/main/java/io/github/steve4744/whatisthis/WhatIsThisListener.java
+++ src/main/java/io/github/steve4744/whatisthis/WhatIsThisListener.java
@@ -30,6 +30,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 public class WhatIsThisListener implements Listener {
 
